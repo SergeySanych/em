5ef2bf5a48bf046ebd69d87021c6592d76e5e49e
SergeySanych <sk@i-fabrika.kz>
2024-02-05 10:33:41 +0600

    05.02.2024 Только Вагтейл

---
+++ em/em/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-om#jwc!k9fqmjcphyi2r$-bp*1*ctsu20x1=4ro*r8w#v3n9ur"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
